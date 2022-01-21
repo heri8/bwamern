c5dccd256985d3d9f837185e173f915b9265f80e
heri setiawan <hsetiawan8.hs@gmail.com>
2022-01-21 21:01:00 +0700

    add component input and breadcrumb

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "assets/scss/style.scss";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPage from "pages/LandingPage";
+import Example from 'pages/Example'
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Route path="/" component={LandingPage}></Route>
+        <Route path="/example" component={Example}></Route>
       </Router>
     </div>
   );
